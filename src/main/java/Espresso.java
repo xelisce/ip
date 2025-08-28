5d90a04af2153fae7335df6aaa795f1a56d65221
xelisce <xelisce@gmail.com>
2025-08-28 17:40:45 +0800

    Add message to echo the user input
    Add class Display to print messages

--- src/main/java/Espresso.java
+++ src/main/java/Espresso.java
@@ -1,13 +1,32 @@
+import java.util.Scanner;
+
 public class Espresso {
+
+    private static void banner() {
+        System.out.println("\n" + Display.LOGO);
+    }
+
+    private static void greeting() {
+        Display.print("Hello! I'm ESPRESSO", "What can I do for you today?");
+    }
+
+    private static void bye() {
+        Display.print("Bye. Hope to see you again soon!");
+    }
+
+    public static void action(String userInput) {
+        Display.print(userInput);
+    }
+
     public static void main(String[] args) {
-        String logo = ".·:''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''':·.\n" +
-                ": : ███████╗███████╗██████╗ ██████╗ ███████╗███████╗███████╗ ██████╗  : :\n" +
-                ": : ██╔════╝██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝██╔═══██╗ : :\n" +
-                ": : █████╗  ███████╗██████╔╝██████╔╝█████╗  ███████╗███████╗██║   ██║ : :\n" +
-                ": : ██╔══╝  ╚════██║██╔═══╝ ██╔══██╗██╔══╝  ╚════██║╚════██║██║   ██║ : :\n" +
-                ": : ███████╗███████║██║     ██║  ██║███████╗███████║███████║╚██████╔╝ : :\n" +
-                ": : ╚══════╝╚══════╝╚═╝     ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝ ╚═════╝  : :\n" +
-                "'·:...................................................................:·'\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner in = new Scanner(System.in);
+        banner();
+        greeting();
+        String userInput = in.nextLine();
+        while (!userInput.equals("bye")) {
+            action(userInput);
+            userInput = in.nextLine();
+        }
+        bye();
     }
 }
